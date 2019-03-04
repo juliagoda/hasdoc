0654f3336a0a127c55753ca690c556149ed68c68
juliagoda <juliagoda.de@gmail.com>
2019-03-04 03:19:17 +0100

    First part of saving states

--- src/Application/Hasdoc/GUI/Menu/Program/StateSave.hs
+++ src/Application/Hasdoc/GUI/Menu/Program/StateSave.hs
@@ -9,6 +9,7 @@ import Graphics.UI.WX
 import qualified Data.Text as T
 import qualified Data.HashMap.Strict as H
 import Data.Ini
+import System.Directory
 
 
 saveFileDialog :: Frame () -> [(String, [String])] -> IO ()
@@ -16,13 +17,25 @@ saveFileDialog mainWindow regex =
     do 
         dirPath <- dirOpenDialog mainWindow True "Wybierz lokalizację dla zapisania postępu" ""
         fileName <- textDialog mainWindow "Pod jaką nazwą chcesz zapisać postęp?" "Podaj nazwę pliku" ""
-         
-        endPath <- fileSaveDialog mainWindow True True "Zapis" regex (getPathDir dirPath) (fileName ++ ".hdoc")
-        let ff = H.fromList [(T.pack "Section1", [( T.pack "1", T.pack "kamień"), (T.pack "2", T.pack "papier"), (T.pack "3", T.pack "nożyce")]), (T.pack "Section2", [(T.pack "1", T.pack "do ręki"), ( T.pack "2", T.pack "do głowy")])]
-        writeIniFile (getPathDir endPath) (Ini {iniGlobals=[(T.pack "A", T.pack "1"),(T.pack "B", T.pack "2")], iniSections=ff}) 
+        home <- getHomeDirectory
+        loadedParser <- readIniFile (home ++ "/.hasdoc-gen/temp/temp.hdoc")
+        case loadedParser of
+             Left a -> infoDialog mainWindow "Brak stanu" "Zmiany w wizardzie nie były wprowadzane"
+             Right b -> case keys (T.pack "Answers") b of
+                             Left c -> infoDialog mainWindow "Brak stanu" "Zmiany w wizardzie nie były wprowadzane"
+                             Right [] -> infoDialog mainWindow "Brak stanu" "Zmiany w wizardzie nie były wprowadzane"
+                             Right d -> do 
+                                 pathChosen <- (fileSaveDialog mainWindow True True "Zapis" regex (getPathDir dirPath) (fileName ++ ".hdoc")) 
+                                 case pathChosen of
+                                      Nothing -> return ()
+                                      Just a -> copyFile (home ++ "/.hasdoc-gen/temp/temp.hdoc") a >> infoDialog mainWindow "Zapis stanu" "Stan został pomyślnie zapisany w wybranej ścieżce"
+                                                          
         return ()
         
         
 getPathDir :: Maybe FilePath -> String
 getPathDir Nothing = ""
 getPathDir (Just x) = x
+
+
+
