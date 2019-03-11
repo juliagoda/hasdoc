5798a511f64fb9c82dd5058dc98188f2b0ebf0c7
Jagoda Górska <juliagoda.de@gmail.com>
2019-03-11 16:38:40 +0100

    Update Hasdoc.hs

--- Hasdoc.hs
+++ Hasdoc.hs
@@ -1,8 +1,6 @@
 module Main where
 import Graphics.UI.WX
 import Application.Hasdoc.GUI.MainWindow (mainwindow)
---import Text.Pandoc.App (convertWithOpts, defaultOpts, options, parseOptions)
---import Text.Pandoc.Error (handleError)
 
 main :: IO ()
 main = start mainwindow
